00ab62a9cd10c47558184d626afd7aabb7be936a
asolo726 <103791071+asolo726@users.noreply.github.com>
2023-02-25 17:39:24 -0600

    1 file modified

--- src/components/TechSkills.js
+++ src/components/TechSkills.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const TechSkills = () =>{
-  return (
-    
-  );
-}
+const TechSkills = () => {
+  return <div className="container"></div>;
+};
 
 export default TechSkills;
